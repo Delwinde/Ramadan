fb6976dbdcc8e73c846b6c46b1f635504ca8e96e
Delwinde <78881131+Delwinde@users.noreply.github.com>
2025-02-20 14:06:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById('hour');
 const minuteEl = document.getElementById('minute');
 const secondEl = document.getElementById('second');
 
-const Ramadan = new Date('March 23, 2023 00:00:00').getTime();
+const Ramadan = new Date('March 01, 2025 00:00:00').getTime();
 
 updateCountdown()
 
